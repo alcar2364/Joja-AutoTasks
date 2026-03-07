cde2dba31d53d4e25e3fd063ed5509610f314b45
alcar2364 <alcarcalimo2364@gmail.com>
2026-03-07 16:59:49 -0500

    Adds core enum for task categories

---
+++ Domain/Tasks/TaskCategory.cs
@@ -0,0 +1,34 @@
+// Defines categories for tasks, used for menu organization and filtering, optional HUD grouping,
+// and other purposes. 
+
+namespace JojaAutoTasks.Domain.Tasks;
+
+// TODO: Expand as needed
+
+public enum TaskCategory
+{
+    Animals,
+    Birthday,
+    SpecialShopOpen,
+    QuestAvailable,
+    Gifting,
+    Talking,
+    CropHarvest,
+    CropWater,
+    TreeHarvest,
+    Mailbox,
+    FestivalActive,
+    FestivalPassive,
+    Pets,
+    QueenOfSauce,
+    MachineReady,
+    FishCaught,
+    SpecialOrdersBoard,
+    QuestTimeSensitive,
+    QuestNormal,
+    Test,
+    ToolPickup,
+    ToolUpgrade,
+    LocationAccessible,
+    LocationInaccessible
+}
