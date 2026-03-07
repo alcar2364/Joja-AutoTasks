d90762e7228f1ec81a02ba8cad4d15f3a4b4e9b0
alcar2364 <alcarcalimo2364@gmail.com>
2026-03-07 16:59:49 -0500

    Adds core enum for task categories

---
+++ Domain/Tasks/TaskCategory.cs
@@ -0,0 +1,16 @@
+// Defines categories for tasks, used for menu organization and filtering, optional HUD grouping,
+// and other purposes. 
+
+namespace JojaAutoTasks.Domain.Tasks;
+
+// TODO: Expand as needed
+
+public enum TaskCategory
+{
+    Farming,
+    Animals,
+    Machines,
+    Social,
+    Exploration,
+    Resources,
+}
