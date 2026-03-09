b02f41273c9f96ca08dd0f2cb89a0f12732f668b
alcar2364 <alcarcalimo2364@gmail.com>
2026-03-09 18:01:34 -0400

    add UncompleteTaskCommand

---
+++ StateStore/Commands/UncompleteTaskCommand.cs
@@ -0,0 +1,23 @@
+using JojaAutoTasks.Domain.Identifiers;
+using JojaAutoTasks.Domain.Tasks;
+
+namespace JojaAutoTasks.StateStore.Commands;
+
+/// <summary>
+/// Command to mark a task as uncompleted.
+/// </summary>
+
+internal sealed class UncompleteTaskCommand : IStateCommand
+{
+    public TaskId TaskId { get; }
+
+    internal UncompleteTaskCommand(TaskId taskId)
+    {
+        if (taskId == default)
+        {
+            throw new ArgumentException("taskId cannot be the default value.", nameof(taskId));
+        }
+
+        TaskId = taskId;
+    }
+}
